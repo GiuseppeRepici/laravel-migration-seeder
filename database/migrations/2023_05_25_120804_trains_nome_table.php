8418619f5bf34e1e0bb78723af73ef99c676cd67
GiuseppeRepici <giuseppe.repici95@gmail.com>
2023-05-25 14:18:27 +0200

    table created

---
+++ database/migrations/2023_05_25_120804_trains_nome_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda', 50);
+            $table->string('Stazione di partenza', 100);
+            $table->string('Stazione di arrivo', 100);
+            $table->time('Orario di partenza');
+            $table->time('Orario di arrivo');
+            $table->smallInteger('Codice Treno')->unsigned();
+            $table->tinyInteger('Numero Carrozze');
+            $table->boolean('In orario')->default(true);
+            $table->boolean('Cancellato')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
